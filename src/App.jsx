06f4f6e09cba5b3c1e1edf3d54a7c88ecb6a40d4
KitsoMogale <156682280+KitsoMogale@users.noreply.github.com>
2024-08-29 14:15:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Layout from "./components/Layout";
+import PickUp from './components/PickUp'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout/>}>
+        
         </Route>
+        <Route path="/pickup" element={<PickUp/>}/>
       </Routes>
     </BrowserRouter>
   );
