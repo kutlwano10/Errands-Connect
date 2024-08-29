cd179a799bbf7c3dc27fc1e35e65ffac7d15b748
Kitso Mogale <kitsomogale19@gmail.com>
2024-08-29 15:44:19 +0200

    .

--- src/components/PickUp.jsx
+++ src/components/PickUp.jsx
@@ -45,7 +45,7 @@ const LaundryPickup = () => {
           <div>
             <label className="block text-sm font-medium text-gray-600">Address</label>
             <input
-              type="text"
+              type="address"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
               className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
